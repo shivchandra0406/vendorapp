3e6fb607a388af591d6722e2a01a84d1fb2f1c5a
shivchandra kumar <shivchandk97@gmail.com>
2021-11-09 16:24:32 +0530

    venderapp

--- vendorapp/admin.py
+++ vendorapp/admin.py
@@ -3,5 +3,5 @@ from .models import *
 # Register your models here.
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
-    list_display=['name','mobilenumber','email','location','service','address','land_link','kyc','accountNumber'
+    list_display=['name','mobilenumber','email','location','service','address','aadharImage','panImage','gstImage','land_line','accountNumber'
     ,'ifsc_code','accountName','bankName','timing','about','logo']
